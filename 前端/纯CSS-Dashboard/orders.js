d2f15ea49a4392bcc2c347cad9058b969fb5b227
Yaner <1468275133@qq.com>
2024-02-17 16:23:22 +0800

    Finish: 纯CSS-Dashboard

---
+++ 前端/纯CSS-Dashboard/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: "Foldable",
+        productNumber: 8563,
+        paymentStatus: "Due",
+        shipping: "Pending",
+    },
+    {
+        productName: "Foldable",
+        productNumber: 8563,
+        paymentStatus: "Due",
+        shipping: "Pending",
+    },
+    {
+        productName: "Foldable",
+        productNumber: 8563,
+        paymentStatus: "Due",
+        shipping: "Pending",
+    }
+]
