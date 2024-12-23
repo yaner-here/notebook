8df0f5f796b8c3a5998bf37f94e675f0ecc8b940
yaner-here <1468275133@qq.com>
2024-12-24 01:00:07 +0800

    update: NixOS Configuration

--- Linux/NixOS.nix
+++ Linux/NixOS.nix
@@ -19,7 +19,31 @@
 	# networking.proxy.default = "http://user:password@proxy:port/";
 	# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 	networking.networkmanager.enable = true;
-	
+
+    # Storage
+    services.btrfs = {
+        autoScrub.enable = true;
+        autoScrub.interval = "weekly";
+    };
+    # services.snapper = {
+    #     snapshotRootOnBoot = true;
+    #     snapshotInterval = "hourly";
+    #     cleanupInterval = "1d";
+    #     persistentTimer = true;
+    #     configs.root = {
+    #         SUBVOLUME = "/";
+    #         ALLOW_USERS = [ "yaner" ];
+    #         ALLOW_GROUPS = [ "root" ];
+    #         TIMELINE_CREATE = true;
+    #         TIMELINE_CLEANUP = true;
+    #         TIMELINE_LIMIT_HOURLY = 1;
+    #         TIMELINE_LIMIT_DAILY = 24;
+    #         TIMELINE_LIMIT_WEEKLY = 1;
+    #         TIMELINE_LIMIT_MONTHLY = 1;
+    #         TIMELINE_LIMIT_YEARLY = 12;
+    #     };
+    # };
+
 	# Font
 	fonts = {
 		enableDefaultPackages = true;
@@ -27,6 +51,7 @@
 		packages = with pkgs; [
 			noto-fonts noto-fonts-cjk-sans noto-fonts-cjk-serif noto-fonts-emoji-blob-bin
 			nerd-fonts.caskaydia-cove
+			cascadia-code
 		];
 		fontconfig = {
 			defaultFonts = {
@@ -38,17 +63,32 @@
 	
 	# Time and Locale
 	time.timeZone = "Asia/Shanghai";
-	i18n.defaultLocale = "zh_CN.UTF-8";
-	i18n.extraLocaleSettings = {
-	    LC_ADDRESS = "zh_CN.UTF-8";
-	    LC_IDENTIFICATION = "zh_CN.UTF-8";
-	    LC_MEASUREMENT = "zh_CN.UTF-8";
-	    LC_MONETARY = "zh_CN.UTF-8";
-	    LC_NAME = "zh_CN.UTF-8";
-	    LC_NUMERIC = "zh_CN.UTF-8";
-	    LC_PAPER = "zh_CN.UTF-8";
-	    LC_TELEPHONE = "zh_CN.UTF-8";
-	    LC_TIME = "zh_CN.UTF-8";
+	i18n = {
+		defaultLocale = "zh_CN.UTF-8";
+		extraLocaleSettings = {
+			LC_ADDRESS = "zh_CN.UTF-8";
+			LC_IDENTIFICATION = "zh_CN.UTF-8";
+			LC_MEASUREMENT = "zh_CN.UTF-8";
+			LC_MONETARY = "zh_CN.UTF-8";
+			LC_NAME = "zh_CN.UTF-8";
+			LC_NUMERIC = "zh_CN.UTF-8";
+			LC_PAPER = "zh_CN.UTF-8";
+			LC_TELEPHONE = "zh_CN.UTF-8";
+			LC_TIME = "zh_CN.UTF-8";
+		};
+		inputMethod = {
+			enable = true;
+			type = "fcitx5";
+			fcitx5.addons = with pkgs; [ 
+				fcitx5-skk fcitx5-lua fcitx5-gtk fcitx5-skk
+				librime fcitx5-rime rime-data
+				fcitx5-fluent
+				fcitx5-chinese-addons
+				fcitx5-pinyin-zhwiki fcitx5-pinyin-moegirl fcitx5-pinyin-minecraft
+				fcitx5-configtool
+			];
+			fcitx5.waylandFrontend = true;
+		};
 	};
 
 	# Desktop Environment
@@ -58,10 +98,20 @@
 	services.xserver.xkb = { layout = "cn"; variant = ""; };
 
 		## >>> Customize: Support Hyper-V to avoiding "no screens found" error of X11 <<<
-		services.xserver.displayManager.gdm.wayland = true;
+		# services.xserver.displayManager.gdm.wayland = true;
 		services.xserver.modules = [ pkgs.xorg.xf86videofbdev ];
-		services.xserver.videoDrivers = [ "hyperv_fb" ];
+		services.xserver.videoDrivers = [ "hyperv_fb" "nvidia" "modesetting" "fbdev" ];
 		users.users.gdm = { extraGroups = [ "video" ]; }; # Also ensure that all users are added to user group "video"!
+
+        hardware.graphics.enable = true;
+        hardware.nvidia.open = false;
+
+
+		qt = {
+			enable = true;
+			style = "adwaita";
+			platformTheme = "kde";
+		};
 		
 		## Hyprland
 		programs.hyprland = {
@@ -171,6 +221,21 @@
     # Clash Verge
     programs.clash-verge = { package = pkgs.clash-verge-rev; enable = true; tunMode = false; autoStart = true; };
 
+    # Docker
+    virtualisation.docker = {
+        enable = true;
+        storageDriver = "btrfs";
+        daemon.settings = {
+            "default-address-pools" = [{ "base" = "172.27.0.0/16"; "size" = 24; }];
+        };
+        autoPrune = {
+            enable = true;
+            dates = "1d";
+            flags = [ "--all" ];
+        };
+    };
+    hardware.nvidia-container-toolkit.enable = true;
+
 	# Firefox
 	programs.firefox.enable = true;
 
@@ -221,7 +286,8 @@
         pkgs.tmux pkgs.waveterm # Shell Emulator
 	    pkgs.rustc pkgs.cargo # Rust
         pkgs.antigen # zsh
-        pkgs.vscode # pkgs.obsidian
+        pkgs.vscode
+		pkgs.obsidian
         pkgs.clash-nyanpasu
 	];
 
