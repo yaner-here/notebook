146d2a0b574b10245ec6943424cda7504af07259
Yaner <noreply@yaner-here.top>
2025-09-27 22:11:34 +0800

    refactor: Remove files

--- 算法/算法板子/popcount.cpp
+++ 算法/算法板子/popcount.cpp
@@ -1,5 +1,27 @@
 #include<cstdint>
 
+/* 原始版本，时间复杂度O(log n) */
+template<typename T> static inline T popcount(T x) {
+    T ans = 0;
+    while(x > 0) {
+        ans += x & 1;
+        x >>= 1;
+    }
+    return ans;
+}
+int popcount(uint64_t x) {
+    int res;
+    for(res = 0; x > 0; x -= x & -x) { ++res; }
+    return res;
+}
+
+/* lowbit加速版本，时间复杂度O(popcount(n)) */
+unsigned int CountOne5(unsigned int x) {
+    unsigned int tmp = x - ((x >> 1) & 033333333333) - ((x >> 2) & 011111111111);
+    return ((tmp + (tmp >> 3)) & 030707070707) % 63;
+}
+
+/* O3优化版本，时间复杂度O(1) */
 static inline uint32_t popcount(uint32_t x) {
     x = (x & 0x55555555u) + ((x >> 1) & 0x55555555u);
     x = (x & 0x33333333u) + ((x >> 2) & 0x33333333u);
@@ -17,17 +39,4 @@ static inline uint64_t popcount(uint64_t x) {
     x = (x & m2) + ((x >> 2) & m2);
     x = (x + (x >> 4)) & m4;
     return (x * h01) >> 56;
-}
-template<typename T> static inline T popcount(T x) {
-    T ans = 0;
-    while(x > 0) {
-        ans += x & 1;
-        x >>= 1;
-    }
-    return ans;
-}
-
-unsigned int CountOne5(unsigned int x) {
-    unsigned int tmp = x - ((x >> 1) & 033333333333) - ((x >> 2) & 011111111111);
-    return ((tmp + (tmp >> 3)) & 030707070707) % 63;
 }
